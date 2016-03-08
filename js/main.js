a0ce6ce198ab91d7e02177ce5128c33c97535e0e
Rachel Glennie <glen0038@algonquinlive.com>
2016-03-08 16:09:07 -0500

    Halfway through

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var $h1 = $('h1');
+var $header = $('header');
+var $main = $('main');
+var $h2 = $('h2');
+var $img = $('img');
+var $dl = $('dl');
+var $dt = $('dt');
+
+$header.addClass('masthead');
+
+$h1.removeClass('bigheading').addClass('logo');
+
+$main.addClass('wrapper');
+
+//#4 here
+
+$h2.addClass('slug-head');
+
+//#6
+
+$img.addClass('slug-img');
+
+//#8, #9
+
+$dl.addClass('classification');
+
+$dt.removeClass('big-label');
